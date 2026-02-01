b0e7dcf6ebd326fd72db78dd9b4cf1ce9828e255
kaytee124 <ktproductions@gmail.com>
2026-02-01 23:01:33 +0000

    learning django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,11 +9,11 @@ from django.views.generic.detail import DetailView
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, 'templates/relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'templates/relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
