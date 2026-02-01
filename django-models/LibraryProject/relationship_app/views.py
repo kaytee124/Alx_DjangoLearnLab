d4360d31cdef105415fca8da5c913fd15f31b14b
kaytee124 <ktproductions@gmail.com>
2026-02-01 22:47:58 +0000

    learning django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 from .models import Book, Author, Library, Librarian
 from django.http import HttpResponse
 
+
 # Create your views here.
 def book_list(request):
     books = Book.objects.all()
     book_list = []
     for book in books:
         book_list.append(f"{book.title} by {book.author.name}")
-    return render(request, 'list_books.html', {'books': book_list})
+    return render(request, 'relationship_app/list_books.html', {'books': book_list})
 
 class LibraryListView(ListView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
