75eeb93fa67abffbf2ee6c5c4a09c97e9b1d4129
kaytee124 <ktproductions@gmail.com>
2026-02-01 23:21:24 +0000

    learning django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
-from .views import login
-from .views import register
-from .views import logout
+from .views import LoginView
+import views
+from .views import LogoutView
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
-    path('logout/', logout, name='logout'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
