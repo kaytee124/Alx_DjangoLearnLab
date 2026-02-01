cea3b74f977ba840e402fae0ebb936aede789ffc
kaytee124 <ktproductions@gmail.com>
2026-02-01 23:15:16 +0000

    learning django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
+from .views import login
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', login, name='login'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,10 @@ from .models import Library
 from django.http import HttpResponse
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
 
 
 # Create your views here.
@@ -20,3 +24,31 @@ class LibraryDetailView(DetailView):
         library = self.get_object()
         books = library.books.all()
         return render(self.request, 'templates/relationship_app/library_detail.html', {'library': library, 'books': books})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse('Login successful')
+        else:
+            return render(request, 'templates/relationship_app/login.html', {'error': 'Invalid username or password'})
+    else:
+        return render(request, 'templates/relationship_app/login.html',{'Error': "Invalid request method"})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        login(request, user)
+        return HttpResponse('User created successfully')
+    else:
+        return render(request, 'templates/relationship_app/register.html', {'Error': "Invalid request method"})
+    
+def logout(request):
+    logout(request)
+    return HttpResponse('Logged out successfully')
