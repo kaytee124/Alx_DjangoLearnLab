c2116eb83b39b1a9f86543e6ea5f42effdfe8d6a
kaytee124 <ktproductions@gmail.com>
2026-02-20 00:29:24 +0000

    new api

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.book_list.as_view(), name='book-list'),
-    path('books/<int:pk>/', views.book_detail.as_view(), name='book-detail'),
-    path('books/create/', views.create_book.as_view(), name='create-book'),
-    path('books/<int:pk>/update/', views.update_book.as_view(), name='update-book'),
-    path('books/<int:pk>/delete/', views.delete_book.as_view(), name='delete-book'),
+    path('books/', views.ListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', views.DetailView.as_view(), name='book-detail'),
+    path('books/create/', views.CreateView.as_view(), name='create-book'),
+    path('books/update/<int:pk>/', views.UpdateView.as_view(), name='update-book'),
+    path('books/delete/<int:pk>/', views.DeleteView.as_view(), name='delete-book'),
 ]
