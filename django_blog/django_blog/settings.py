51f2a98e5d0aa9e0407a80094135896741c6cab1
kaytee124 <ktproductions@gmail.com>
2026-02-21 21:09:31 +0000

    new api

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_db',
+        'USER': 'root',
+        'PASSWORD': 'Vicky@2017',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
